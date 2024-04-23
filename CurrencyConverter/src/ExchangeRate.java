37f50015bf05e055fd5e5060d5822893c6816a4c
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-04-23 13:58:44 -0500

    currency converter

---
+++ CurrencyConverter/src/ExchangeRate.java
@@ -0,0 +1,19 @@
+public class ExchangeRate {
+    static void converter(byte option, double value){
+        System.out.println("==================================================");
+        System.out.println("option: "+ option);
+        System.out.println("value: "+ value);
+        System.out.println("==================================================");
+    }
+}
+/*
+System.out.print("Usted ha seleccionado: ");
+System.out.println("Dólar =>> Peso Argentino");
+System.out.println("Peso Argentino =>> Dólar");
+System.out.println("Dólar =>> Real Brasileño");
+System.out.println("Real Brasileño =>> Dólar");
+System.out.println("Dólar =>> Peso Colombiano");
+System.out.println("Peso Colombiano =>> Dólar");
+El valor xx.x [USD] corresponde al valor final de =>>> xx.x [ARS]
+El valor xx.x [BRL] corresponde al valor final de =>>> xx.x [USD]
+*/
